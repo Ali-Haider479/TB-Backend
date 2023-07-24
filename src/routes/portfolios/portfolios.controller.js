7f8219bb79110d01d29e4075a86d3102ff0a8c3e
Bazil Sajjad <bazil@Bazils-MacBook-Air.local>
2023-07-24 23:41:58 +0500

    Speed up

--- src/routes/portfolios/portfolios.controller.js
+++ src/routes/portfolios/portfolios.controller.js
@@ -344,98 +344,194 @@ const getExchangeAsset = async (exchange) => {
     if (exchange?.exchangeType === "Binance Spot") {
       console.log("Binance Spot Block");
       let data = await client.getBalances();
-      // console.log("Result from server: ", data);
       result = data.filter((item) => parseFloat(item.free) > 0);
-
       console.log("getBalance result: ", result);
     } else {
       result = await client.getBalance();
       console.log("getBalance result: ", result);
     }
-    // const result = await client.getBalance();
-    // console.log("getBalance result: ", result);
 
     const transformedResult = [];
 
-    if (exchange?.exchangeType === "Binance Spot") {
-      console.log("spot is", result);
-      // const newResult = result.filter(exchange=> exchange.free !== "0")
-      for (const asset of result) {
-        if (asset.coin === "USDT") {
-          asset["usdt_price"] = +asset.free;
-          asset["asset"] = asset.coin;
-          asset["balance"] = asset.free;
-        } else {
-          const symbol = `${asset.coin}/USDT`;
-          console.log(symbol);
-          try {
-            const ticker = await binance.fetchTicker(symbol);
-
-            // Add the check here
-            if (ticker === undefined) {
-              console.log(`Ticker for ${symbol} is undefined.`);
-              continue; // Skip to the next iteration
-            }
-
-            const usdtPrice = ticker.last;
-            const usdtBalance = parseFloat(asset.free) * usdtPrice;
-            asset["usdt_price"] = usdtBalance;
-            asset["asset"] = asset.coin;
-            asset["balance"] = asset.free;
-          } catch (err) {
-            console.log(err);
-            const symbol = `${asset.coin}/BUSD`;
-            console.log(symbol);
-            try {
-              const ticker = await binance.fetchTicker(symbol);
-
-              // Add the check here too
-              if (ticker === undefined) {
-                console.log(`Ticker for ${symbol} is undefined.`);
-                continue; // Skip to the next iteration
-              }
-
-              const usdtPrice = ticker.last;
-              const usdtBalance = parseFloat(asset.free) * usdtPrice;
-              asset["usdt_price"] = usdtBalance;
-              asset["asset"] = asset.coin;
-              asset["balance"] = asset.free;
-            } catch (error) {
-              console.log(error);
-            }
-          }
-        }
-        const { asset: coin_name, balance: quantity, usdt_price } = asset;
-        if (
-          coin_name !== undefined &&
-          quantity !== undefined &&
-          usdt_price !== undefined &&
-          !isNaN(usdt_price)
-        ) {
-          transformedResult.push({ coin_name, quantity, usdt_price });
-        }
+    // prepare fetch promises
+    const fetchPromises = result.map((asset) => {
+      if (asset.coin === "USDT" || asset.asset === "USDT") {
+        return Promise.resolve({ asset: asset.coin || asset.asset, usdt_price: asset.balance });
       }
-    } else {
-      for (const asset of result) {
-        if (asset.asset === "USDT") {
-          asset["usdt_price"] = asset.balance;
-        } else {
-          const symbol = `${asset.asset}/USDT`;
-          const ticker = await binance.fetchTicker(symbol);
-          const usdtPrice = ticker.last;
-          const usdtBalance = parseFloat(asset.balance) * usdtPrice;
-          asset["usdt_price"] = usdtBalance;
-        }
-        const { asset: coin_name, balance: quantity, usdt_price } = asset;
-        transformedResult.push({ coin_name, quantity, usdt_price });
+      const baseSymbol = `${asset.coin || asset.asset}/USDT`;
+      const alternativeSymbol = `${asset.coin || asset.asset}/BUSD`;
+
+      return binance.fetchTicker(baseSymbol).catch((err) => {
+        console.log(`Error fetching ${baseSymbol}. Trying ${alternativeSymbol}...`);
+        return binance.fetchTicker(alternativeSymbol);
+      });
+    });
+
+    const tickers = await Promise.all(fetchPromises);
+
+    tickers.forEach((ticker, index) => {
+      const asset = result[index];
+      const coin_name = asset.coin || asset.asset;
+      const quantity = asset.free || asset.balance;
+
+      if (ticker === undefined) {
+        console.log(`Ticker for ${coin_name} is undefined.`);
+        return; // Skip to the next iteration
       }
-    }
-    // console.log(transformedResult);
+
+      const usdtPrice = ticker.last;
+      const usdtBalance = parseFloat(quantity) * usdtPrice;
+
+      if (
+        coin_name !== undefined &&
+        quantity !== undefined &&
+        usdtPrice !== undefined &&
+        !isNaN(usdtPrice)
+      ) {
+        transformedResult.push({ coin_name, quantity, usdt_price: usdtBalance });
+      }
+    });
+
     return transformedResult;
   } catch (err) {
     console.error("getBalance error: ", err);
   }
 };
+
+
+// const getExchangeAsset = async (exchange) => {
+//   console.log("get assets", exchange);
+//   let client;
+
+//   if (exchange?.exchangeType === "Binance Futures Testnet") {
+//     console.log("Testnet");
+//     const baseUrl = "https://testnet.binancefuture.com";
+//     client = new USDMClient({
+//       api_key: exchange?.apiKey,
+//       api_secret: exchange?.secretKey,
+//       baseUrl,
+//       recvWindow: 10000,
+//     });
+//   }
+//   if (exchange?.exchangeType === "Binance Futures") {
+//     console.log("Futures");
+//     client = new USDMClient({
+//       api_key: exchange?.apiKey,
+//       api_secret: exchange?.secretKey,
+//       recvWindow: 10000,
+//     });
+//   }
+//   if (exchange?.exchangeType === "Binance Spot") {
+//     console.log("Spot");
+//     client = new MainClient({
+//       api_key: exchange?.apiKey,
+//       api_secret: exchange?.secretKey,
+//       recvWindow: 10000,
+//     });
+//   }
+
+//   const binance = new ccxt.binance();
+
+//   try {
+//     let result;
+//     if (exchange?.exchangeType === "Binance Spot") {
+//       console.log("Binance Spot Block");
+//       let data = await client.getBalances();
+//       // console.log("Result from server: ", data);
+//       result = data.filter((item) => parseFloat(item.free) > 0);
+
+//       console.log("getBalance result: ", result);
+//     } else {
+//       result = await client.getBalance();
+//       console.log("getBalance result: ", result);
+//     }
+//     // const result = await client.getBalance();
+//     // console.log("getBalance result: ", result);
+
+//     const transformedResult = [];
+
+//     if (exchange?.exchangeType === "Binance Spot") {
+//       console.log("spot is", result);
+//       // const newResult = result.filter(exchange=> exchange.free !== "0")
+//       for (const asset of result) {
+//         if (asset.coin === "USDT") {
+//           asset["usdt_price"] = +asset.free;
+//           asset["asset"] = asset.coin;
+//           asset["balance"] = asset.free;
+//         } 
+//         else {
+//           const symbol = `${asset.coin}/USDT`;
+//           console.log(symbol);
+//           try {
+//             const ticker = await binance.fetchTicker(symbol);
+
+//             // Add the check here
+//             if (ticker === undefined) {
+//               console.log(`Ticker for ${symbol} is undefined.`);
+//               continue; // Skip to the next iteration
+//             }
+
+//             const usdtPrice = ticker.last;
+//             const usdtBalance = parseFloat(asset.free) * usdtPrice;
+//             asset["usdt_price"] = usdtBalance;
+//             asset["asset"] = asset.coin;
+//             asset["balance"] = asset.free;
+//           } catch (err) {
+//             console.log(err);
+//             const symbol = `${asset.coin}/BUSD`;
+//             console.log(symbol);
+//             try {
+//               const ticker = await binance.fetchTicker(symbol);
+
+//               // Add the check here too
+//               if (ticker === undefined) {
+//                 console.log(`Ticker for ${symbol} is undefined.`);
+//                 continue; // Skip to the next iteration
+//               }
+
+//               const usdtPrice = ticker.last;
+//               const usdtBalance = parseFloat(asset.free) * usdtPrice;
+//               asset["usdt_price"] = usdtBalance;
+//               asset["asset"] = asset.coin;
+//               asset["balance"] = asset.free;
+//             } catch (error) {
+//               console.log(error);
+//             }
+//           }
+//         }
+//         const { asset: coin_name, balance: quantity, usdt_price } = asset;
+//         if (
+//           coin_name !== undefined &&
+//           quantity !== undefined &&
+//           usdt_price !== undefined &&
+//           !isNaN(usdt_price)
+//         ) {
+//           transformedResult.push({ coin_name, quantity, usdt_price });
+//         }
+//       }
+
+      
+//     } else {
+//       for (const asset of result) {
+//         if (asset.asset === "USDT") {
+//           asset["usdt_price"] = asset.balance;
+//         } else {
+//           const symbol = `${asset.asset}/USDT`;
+//           const ticker = await binance.fetchTicker(symbol);
+//           const usdtPrice = ticker.last;
+//           const usdtBalance = parseFloat(asset.balance) * usdtPrice;
+//           asset["usdt_price"] = usdtBalance;
+//         }
+//         const { asset: coin_name, balance: quantity, usdt_price } = asset;
+//         transformedResult.push({ coin_name, quantity, usdt_price });
+//       }
+//     }
+//     // console.log(transformedResult);
+//     return transformedResult;
+//   } catch (err) {
+//     console.error("getBalance error: ", err);
+//   }
+// };
 
 const getExchangeAssetSymbol = async (exchange) => {
   console.log("get assets", exchange);
